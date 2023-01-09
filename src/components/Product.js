726eefc73487ee4f2666da59e5526046374ef6fc
YishaiHaim <97115878+YishaiHaim@users.noreply.github.com>
2023-01-09 18:21:03 +0200

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,7 +13,7 @@ function Product({ product }) {
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={`https://deployanimeeecom.onrender.com${product.image}`} />
+        <Card.Img src={`https://deployanimeecom-lt0t.onrender.com${product.image}`} />
       </Link>
 
       <Card.Body>
